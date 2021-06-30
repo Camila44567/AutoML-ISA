4b43180eb6bdd569b7408b4f52f4fa916c379a01
Camila Moreno <camilacastromoreno1994@gmail.com>
2021-06-30 18:34:07 -0300

    Added getIntervals function from AEM

--- autoextractionmethod.py
+++ autoextractionmethod.py
@@ -1,7 +1,135 @@
 import operator
 import pandas as pd
+from collections import defaultdict
 from IPython.display import display_html
 from itertools import chain,cycle
+
+# This function is used to get intervals of Good and Bad instances
+# for each dataset
+def getIntervals(datasets_dict):
+    
+    # list of dataset names
+    dataset_names = list(datasets_dict['original_features'].keys())
+
+    # Let's define the good and bad interval dictionary
+    G = {}
+    B = {}
+
+
+    # Let's go through each dataset
+    # We'll be evaluating the domains of competence of each algorithm within each dataset
+    for dataset in dataset_names:
+
+        # Let's create an empty dictionary for this dataset
+        G[dataset] = {}
+        B[dataset] = {}
+
+        # Let's get the algorithm names for the current dataset
+        algorithm_names = list(datasets_dict['algorithm_bin'][dataset].columns)
+
+        # Let's go through each class (algorithm performance)
+        for algorithm in algorithm_names:
+
+            # Let's create an empty dictionary for this algorithm in this dataset
+            G[dataset][algorithm] = {}
+            B[dataset][algorithm] = {}
+
+            # Let's get the meta feature names for the current dataset
+            meta_feature_names = list(datasets_dict['meta_features'][dataset].columns)
+
+
+            # let's go through each column of the meta features (MFj)
+            for metafeature in meta_feature_names:
+
+                # Let's create an empty dictionary for this metafeature 
+                # in this algorithm in this dataset
+                G[dataset][algorithm][metafeature] = {}
+                B[dataset][algorithm][metafeature] = {}
+
+
+                # let's create an auxiliary empty list of good and bad interval indexes and values 
+                G_int_ind = []
+                G_int_val = []
+                B_int_ind = []
+                B_int_val = []
+
+
+                # add MFj that will be used for sorting to dataset and 
+                # algorithm performance from algorithm_bin to define good/bad elements 
+                U = datasets_dict['original_features'][dataset].assign(
+                    MFj = datasets_dict['meta_features'][dataset][metafeature], 
+                    Algorithm_Perf = datasets_dict['algorithm_bin'][dataset][algorithm])
+
+                # sort list U by each meta feature MFj
+                UMFj = U.sort_values(by=['MFj'])
+                UMFj.reset_index(inplace=True)
+
+                # let's search for good behavior intervals
+                i = 0
+                pos = 0
+                while i < len(UMFj)-1 and pos != -1:
+
+                    # position of the next good instance
+                    pos = nextGoodInstance(i, UMFj)
+
+                    if pos != -1:
+                        V_ind, V_val, i = extendGoodInterval(pos, UMFj)
+                        G_int_ind.append(V_ind)
+                        G_int_val.append(V_val)
+
+                # let's search for bad behavior intervals
+                i = 0
+                pos = 0
+                while i < len(UMFj)-1 and pos != -1:
+
+                    # position of the next bad instance
+                    pos = nextBadInstance(i, UMFj)
+                    if pos != -1:
+                        V_ind, V_val, i = extendBadInterval(pos, UMFj)
+                        B_int_ind.append(V_ind)
+                        B_int_val.append(V_val)
+
+                # Let's merge and filter the intervals if necessary
+                G_int_ind, G_int_val = mergeIntervals(G_int_ind, G_int_val)
+                B_int_ind, B_int_val = mergeIntervals(B_int_ind, B_int_val)
+
+                G_int_ind, G_int_val = dropSmallIntervals(G_int_ind, G_int_val, len(UMFj))
+                B_int_ind, B_int_val = dropSmallIntervals(B_int_ind, B_int_val, len(UMFj))
+
+                # keep Good interval with the largest support
+                if G_int_ind:
+                    G_int_ind_aux = G_int_ind[0]
+                    G_int_val_aux = G_int_val[0]
+
+                    for i in range(len(G_int_ind)):
+                        if G_int_ind[i][1] - G_int_ind[i][0] > G_int_ind_aux[1] - G_int_ind_aux[0]:
+                            G_int_ind_aux = G_int_ind[i]
+                            G_int_val_aux = G_int_val[i]
+                else: 
+                    G_int_ind_aux = []
+                    G_int_val_aux = []
+
+                # keep Bad interval with the largest support
+                if B_int_ind:
+                    B_int_ind_aux = B_int_ind[0]
+                    B_int_val_aux = B_int_val[0]
+
+                    for i in range(len(B_int_ind)):
+                        if B_int_ind[i][1] - B_int_ind[i][0] > B_int_ind_aux[1] - B_int_ind_aux[0]:
+                            B_int_ind_aux = B_int_ind[i]
+                            B_int_val_aux = B_int_val[i]
+                else: 
+                    B_int_ind_aux = []
+                    B_int_val_aux = []
+
+                # Add G_aux and B_aux to dictionaries according to the current 
+                # dataset, algorithm, metafeature and interval        
+                G[dataset][algorithm][metafeature]['interval_ind'] = G_int_ind_aux
+                G[dataset][algorithm][metafeature]['interval_val'] = G_int_val_aux
+                B[dataset][algorithm][metafeature]['interval_ind'] = B_int_ind_aux
+                B[dataset][algorithm][metafeature]['interval_val'] = B_int_val_aux
+    
+    return G, B
 
 
 # This function is used to determine the next good instance j (Algorithmic Performance == 1)
